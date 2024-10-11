b841265e6d0b3b20b7833e3ff8d54b9fcc6afd36
t2023-m0106 <t2023-m0106@t2023-m0106ui-MacBookAir.local>
2024-10-11 17:33:04 +0900

    add: focuson init

---
+++ src/app/homepage/FocusOn.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FocusOn = () => {
+  return (
+    <div>
+      <h1>오늘 가장 집중할 일</h1>
+      <input />{' '}
+    </div>
+  );
+};
+
+export default FocusOn;
--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Timer from './timer';
+import FocusOn from './focusOn';
 
 const HomePage = () => {
   return (
     <div>
       <h1>homepage</h1>
       <Timer />
+      <FocusOn />
     </div>
   );
 };
