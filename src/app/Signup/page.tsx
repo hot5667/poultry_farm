fda8eafeabc8a7dd9800d24cdbd4fe777a16cfe7
김도현 <hot99009@gmail.com>
2024-10-12 03:44:29 +0900

    [add] lib 파일 추가 슈파베이스 클리이언트 생성

---
+++ src/app/Signup/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '회원가입 페이지',
+  description: '회원가입의 간단한 설명입니다.',
+};
+
+const SignPage = () => {
+  return <div>회원가입 페이지 입니다.</div>;
+};
+
+export default SignPage;
