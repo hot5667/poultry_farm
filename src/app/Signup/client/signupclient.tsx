ce21ca1861edb1729d52facdfdda810e66f96db4
김도현 <hot99009@gmail.com>
2024-10-12 03:51:41 +0900

    [add] autoAuth추가

--- src/app/Signup/client/signupclient.tsx
+++ src/app/Signup/client/signupclient.tsx
@@ -2,3 +2,17 @@
 
 import { useState } from "react"
 import { useRouter } from 'next/navigation';
+
+const SignClient = () => {
+     const router = useRouter();
+     const [email, setEmail] = useState('');
+     const [password, setPassword] = useState('');
+
+     return (
+          <div>
+               <h1>
+                    
+               </h1>
+          </div>
+     )
+}
