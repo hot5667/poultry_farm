5b58e547c7c0a1d6ce86f3fd7e05ac68b26fc737
Bsjaehee <bsjaehee94@gmail.com>
2024-10-14 17:48:57 +0900

    add: 로그인한 유저 정보 불러오기

--- src/app/mypage/_components/Profile.tsx
+++ src/app/mypage/_components/Profile.tsx
@@ -1,8 +1,14 @@
 'use client';
+import browserClient from '@/util/supabase/client';
+import { User } from '@supabase/supabase-js';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 
-const Profile = () => {
+interface ProfileProps {
+  user: User;
+}
+
+const Profile = ({ user }: ProfileProps) => {
   return (
     <div className="w-[200px] h-auto p-2 md:ml-7">
       <Image
@@ -14,12 +20,12 @@ const Profile = () => {
       />
       <div className="flex flex-col items-center">
         <div className="flex flex-row items-baseline justify-center mt-2">
-          <h2 className="font-black text-2xl md:text-3xl">치킨</h2>
+          <h2 className="font-black text-2xl md:text-3xl">{user.email}</h2>
           <span>님,</span>
         </div>
 
         <p className="mt-1">오늘도 화이팅!</p>
-        <p className="text-gray-300 font-medium">test@test.com</p>
+        <p className="text-gray-300 font-medium">{user.email}</p>
 
         <button>수정</button>
       </div>
