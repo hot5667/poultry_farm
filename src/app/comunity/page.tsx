fd15ff354feae0e18454b617f96023d6e2bce26b
lim <dntksql1010@naver.com>
2024-10-14 12:53:02 +0900

    add supabase 연동

--- src/app/comunity/page.tsx
+++ src/app/comunity/page.tsx
@@ -1,56 +1,9 @@
-import Link from 'next/link';
-
-const MOCK_DATA = [
-  {
-    user_id: 1,
-    username: 'challenge_master',
-    challenge_name: '30-Day Fitness Challenge',
-    progress: 80,
-    start_date: '2024-09-01',
-    end_date: '2024-09-30',
-    current_day: 24,
-    completed: false,
-  },
-  {
-    user_id: 2,
-    username: 'fit_lover',
-    challenge_name: 'Healthy Eating Challenge',
-    progress: 100,
-    start_date: '2024-08-15',
-    end_date: '2024-09-15',
-    current_day: 30,
-    completed: true,
-  },
-  {
-    user_id: 3,
-    username: 'goal_digger',
-    challenge_name: 'Reading Challenge - 12 Books',
-    progress: 50,
-    start_date: '2024-10-01',
-    end_date: '2024-12-31',
-    current_day: 15,
-    completed: false,
-  },
-  {
-    user_id: 4,
-    username: 'life_changer',
-    challenge_name: '5K Running Challenge',
-    progress: 60,
-    start_date: '2024-09-20',
-    end_date: '2024-10-20',
-    current_day: 18,
-    completed: false,
-  },
-];
+import Post from './_components/Post';
 
 export default function Comunity() {
-  const data = MOCK_DATA;
-
   const now = new Date();
   const month = now.getMonth() + 1;
   const day = now.getDate();
-
-  console.log(month);
 
   return (
     <div>
@@ -58,21 +11,7 @@ export default function Comunity() {
         {month}월{day}일
       </h3>
       <div>
-        {data.map((user) => (
-          <div className="mb-[20px]" key={user.user_id}>
-            <Link href={`/`}>
-              <strong>{user.username}</strong>
-              <p>{user.challenge_name}</p>
-              <span>진행률: {user.progress}</span>
-              <ul>
-                <li>{user.start_date}</li>
-                <li>{user.end_date}</li>
-              </ul>
-              <span>{user.current_day}</span>
-              <strong>{user.completed}</strong>
-            </Link>
-          </div>
-        ))}
+        <Post />
       </div>
     </div>
   );
