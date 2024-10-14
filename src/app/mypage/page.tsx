b8a6b8bf8ab069ccd01b8a2fb4697a1e0d2d4d94
Bsjaehee <bsjaehee94@gmail.com>
2024-10-15 00:57:19 +0900

    add: 마이페이지 카드 진행률 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,13 +1,33 @@
+import { createClient } from '@/util/supabase/server';
+import Card from './_components/Card';
 import Heatmap from './_components/Heatmap';
 import Profile from './_components/Profile';
 
 const MyPage = async () => {
+  const supabase = createClient();
+  let { data, error } = await supabase.auth.signInWithPassword({
+    email: 'test12@test.com',
+    password: 'test12@test.com',
+  });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  // console.log(user);
+  if (!user) return;
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) return;
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-3">
       {/* 프로필 섹션 */}
       <section className="md:col-span-1 place-items-center md:mt-16">
         <div className="flex flex-col justify-center items-center">
-          <Profile />
+          <Profile user={user} session={session} />
         </div>
       </section>
 
@@ -17,6 +37,9 @@ const MyPage = async () => {
       </section>
 
       {/* 카드 섹션 */}
+      <section className="col-span-3">
+        <Card user={user} />
+      </section>
     </div>
   );
 };
