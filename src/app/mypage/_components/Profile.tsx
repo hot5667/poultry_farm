10629fc4730a87c5dfb8ab3206986ca5ed5a561f
Bsjaehee <bsjaehee94@gmail.com>
2024-10-14 21:44:31 +0900

    add: 닉네임 수정

--- src/app/mypage/_components/Profile.tsx
+++ src/app/mypage/_components/Profile.tsx
@@ -1,26 +1,71 @@
 'use client';
-import { User } from '@supabase/supabase-js';
+import browserClient from '@/util/supabase/client';
+import { Session, User } from '@supabase/supabase-js';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import Image from 'next/image';
 import React, { useState } from 'react';
+import AccountForm from './ProfileImage';
+import Avatar from './ProfileImage';
 
 interface ProfileProps {
   user: User;
+  session: Session;
 }
 
-const Profile = ({ user }: ProfileProps) => {
+const Profile = ({ user, session }: ProfileProps) => {
   const [isEditing, setIsEditing] = useState(false);
-  const [nickname, setNickname] = useState(
-    user.user_metadata?.nickname || user.email
-  );
-
+  const [nickname, setNickname] = useState(user.email || '');
+  const queryClient = useQueryClient();
+  // User 테이블 데이터 가져오기
+  const { data: users, error: usersError } = useQuery({
+    queryKey: ['users'],
+    queryFn: async () => {
+      const { data, error } = await browserClient.from('User').select('*');
+      return data;
+    },
+  });
+  // 닉네임 수정 form 열기
   const handleEdit = () => {
     setIsEditing(true);
   };
-
-  const handleOut = () => {
+  // 닉네임 수정 form 닫기
+  const handleCancel = () => {
     setIsEditing(false);
   };
 
+  // 닉네임 변경
+  const updateUser = async (newNickname: string) => {
+    if (!session) {
+      throw new Error('사용자가 인증되지 않았습니다.');
+    }
+    const { data, error } = await browserClient
+      .from('User')
+      .update({ NickName: newNickname })
+      .eq('UserID', user.id)
+      .select();
+    if (error) {
+      throw new Error(`닉네임 수정 오류: ${error.message}`);
+    }
+    setNickname(newNickname);
+    setIsEditing(false);
+  };
+  const { mutate } = useMutation({
+    mutationFn: updateUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['users'],
+      });
+    },
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutate(nickname);
+  };
+
+  const loggedInUser = users?.find((e) => e.UserID === user.id);
+  console.log('유저정보--------->', user);
+  // console.log(loggedInUser);
   return (
     <div className="w-[200px] h-auto p-2 md:ml-7">
       <Image
@@ -32,7 +77,9 @@ const Profile = ({ user }: ProfileProps) => {
       />
       <div className="flex flex-col items-center">
         <div className="flex flex-row items-baseline justify-center mt-2">
-          <h2 className="font-black text-2xl md:text-3xl">{user.email}</h2>
+          <h2 className="font-black text-2xl md:text-3xl">
+            <p>{loggedInUser?.NickName}</p>
+          </h2>
           <span>님,</span>
         </div>
 
@@ -40,14 +87,21 @@ const Profile = ({ user }: ProfileProps) => {
         <p className="text-gray-300 font-medium">{user.email}</p>
 
         {isEditing ? (
-          <form>
+          <form onSubmit={handleSubmit}>
             <input
               className="border border-gray-500"
               type="text"
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
               placeholder="새로운 닉네임을 입력하세요"
             />
-            <button className="p-2 border border-gray-500">저장</button>
-            <button onClick={handleOut} className="p-2 border border-gray-500">
+            <button type="submit" className="p-2 border border-gray-500">
+              저장
+            </button>
+            <button
+              onClick={handleCancel}
+              className="p-2 border border-gray-500"
+            >
               취소
             </button>
           </form>
