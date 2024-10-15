86377a2a18be6cb41ed280950d64985911c7b37c
lim <dntksql1010@naver.com>
2024-10-16 01:43:15 +0900

    add 검색 기능 구현

--- src/app/comunity/_components/Comment.tsx
+++ src/app/comunity/_components/Comment.tsx
@@ -10,23 +10,20 @@ interface id {
 }
 
 const Comment = ({ feedID }: id) => {
-  const [isLogin, setLogin] = useState();
   const [comment, setComment] = useState<string>('');
   const { data, isLoading, isError } = getCommentQuery();
   const { data: user } = getCommentUserInfo();
 
   const { mutate } = useAddMutation();
 
+  console.log(feedID);
+
   const handleComment = (e: { target: { value: SetStateAction<string> } }) => {
     setComment(e.target.value);
   };
 
-  // console.log(data);
-  // console.log('////');
-  // console.log(user);
-
-  if (!data || !user) return <h2>데이터가 없습니다.</h2>;
-
+  if (!user) return <h2>댓글을 달려면 로그인을 해주세요</h2>;
+  if (!data) return <h2>데이터 오류</h2>;
   if (isError) return <h2>데이터를 불러오지 못했습니다.</h2>;
 
   return (
@@ -47,7 +44,7 @@ const Comment = ({ feedID }: id) => {
           value={comment}
           onChange={handleComment}
         />
-        {isLogin ? null : <button type="submit">추가하기</button>}
+        <button type="submit">추가하기</button>
       </form>
     </div>
   );
--- src/app/comunity/_components/Post.tsx
+++ src/app/comunity/_components/Post.tsx
@@ -1,45 +1,84 @@
 'use client';
 
 import Comment from './Comment';
-import { getComunityInfo } from '@/quries/useGetComunityQuery';
 import CommentButton from './CommentButton';
+import { SetStateAction, useState } from 'react';
+import browserClient from '@/util/supabase/client';
+import { feed } from '@/type/comunity';
+import { useQuery } from '@tanstack/react-query';
 
 const Post = () => {
-  const { data, isError } = getComunityInfo();
+  const [mysearch, setSearch] = useState('');
+  async function fetchSearchPost(): Promise<feed[]> {
+    const { data, error } = await browserClient
+      .from('feed')
+      .select('*, User(*), Comment(*)')
+      .ilike('Category', `%${mysearch}%`);
+    if (error) {
+      throw new Error();
+    } else {
+      return data;
+    }
+  }
+
+  const { data, isError, isLoading, refetch, isFetching } = useQuery<feed[]>({
+    queryKey: ['post'],
+    queryFn: fetchSearchPost,
+  });
+
   console.log(data);
 
-  if (!data) return <h2>데이터가 없습니다</h2>;
+  const handleSearch = (e: { target: { value: SetStateAction<string> } }) => {
+    setSearch(e.target.value);
+  };
 
+  function onClickSearch() {
+    refetch();
+  }
+
+  if (isLoading) return <h2>데이터 로딩중</h2>;
+  if (!data) return <h2>데이터가 없습니다</h2>;
   if (isError) return <h2>데이터를 불러오지 못했습니다.</h2>;
 
-  console.log(data);
   return (
-    <div>
-      {data?.map((post) => (
-        <div key={post.User_ID}>
-          <strong>{post.User?.NickName}</strong>
-          <span>{post.User?.User_Challenge}</span>
-          <p>{post.Category}</p>
-          <p>{post.Feed_Content}</p>
-          <ul>
-            <li>시작날: {post.Challenge_start_progress}</li>
-            <li>종료날: {post.Challenge_end_progress}</li>
-          </ul>
-          {post.Comment.map((comment) => (
-            <div key={comment.Comment_ID}>
-              <div>
-                <p>{comment.Comment_Content}</p>
+    <>
+      <input
+        className="border"
+        type="text"
+        value={mysearch}
+        onChange={handleSearch}
+      />
+      <button onClick={onClickSearch}>검색하기</button>
+      <div className="flex gap-16 m-[20px]">
+        {data?.map((post) => (
+          <div className="p-[20px] border" key={post.User_ID}>
+            <strong className="block text-[32px]">{post.Category}</strong>
+            <strong className="block text-[32px]">{post.User?.NickName}</strong>
+            <span className="text-[24px] mb-[10px] mt-[10px] block">
+              {post.User?.User_Challenge}
+            </span>
+            <p>{post.Category}</p>
+            <p>{post.Feed_Content}</p>
+            <ul>
+              <li>시작날: {post.Challenge_start_progress}</li>
+              <li>종료날: {post.Challenge_end_progress}</li>
+            </ul>
+            {post.Comment.map((comment) => (
+              <div className="flex" key={comment.Comment_ID}>
+                <div>
+                  <p>{comment.Comment_Content}</p>
+                </div>
+                <CommentButton
+                  id={comment.Comment_ID}
+                  userID={comment.User_ID}
+                />
               </div>
-              <CommentButton
-                id={comment.Comment_ID}
-                text={comment.Comment_Content}
-              />
-            </div>
-          ))}
-          <Comment feedID={post.User_feed_ID} />
-        </div>
-      ))}
-    </div>
+            ))}
+            <Comment feedID={post.User_feed_ID} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
